49c46c8b0616fc7e8cd08d364628cc4523bec8b6
Evening <johnchangqi@hotmail.com>
2022-12-03 15:15:58 +0800

    Conditionally extract files to reduce extract processing time

--- src/download.py
+++ src/download.py
@@ -1,8 +1,10 @@
 import logging
 import tarfile
 from pathlib import Path
+from typing import List
 
 import wget
+from tqdm import tqdm
 
 
 def download_file(url: str, fn: Path, overwrite=False) -> bool:
@@ -19,25 +21,27 @@ def download_file(url: str, fn: Path, overwrite=False) -> bool:
     return True
 
 
-def unzip_tar_bz2(fn: Path):
+def unzip_tar_bz2(fn: Path, sql_names: List[str]):
     """ Unzips the file"""
     logging.info(f"Unzipping File {fn}")
     with tarfile.open(fn.as_posix(), "r:bz2") as tar:
-        tar.extractall(fn.parent)
+        for sql_name in tqdm(sql_names, desc='Extracting Files'):
+            tar.extract(sql_name, fn.parent.as_posix())
 
 
-def download_pipeline(url: str, fn: Path, overwrite=False, cleanup=False):
+def download_pipeline(url: str, fn: Path, sql_names: List[str], overwrite=False, cleanup=False):
     """ Downloads the database files
 
     Args:
         url: https://data.ppy.sh... url
         fn: File Name to download to
+        sql_names: SQL files to extract
         overwrite: Whether to overwrite the tar gz2
         cleanup: Deletes the tar gz2
 
     """
     if download_file(url, fn, overwrite):
-        unzip_tar_bz2(fn)
+        unzip_tar_bz2(fn, sql_names)
         if cleanup:
             logging.info(f"Cleaning up file {fn}")
             fn.unlink()
