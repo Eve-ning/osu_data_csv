9d36e68d0cddcbe4c3837fba25c354a3f31a6cf0
Evening <johnchangqi@hotmail.com>
2022-12-03 15:25:04 +0800

    Clean up repo

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import logging
-import tarfile
-
-from tqdm import tqdm
 
 from src.conf import DATA_DIR
-from src.download import download_pipeline
-from src.sql_to_df import sql_to_df
+from src.pipelines import download_pipeline, convert_pipeline
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
@@ -17,25 +13,10 @@ if __name__ == '__main__':
     fn_dir_csv.mkdir(parents=True, exist_ok=True)
     fn_url = fr"https://data.ppy.sh/{fn_tar}"
 
-    download_pipeline(fn_url, DATA_DIR / fn_tar, overwrite=False, cleanup=False)
-
     sql_names = [
         "osu_user_stats_mania.sql",
         "osu_user_beatmap_playcount.sql"
     ]
 
-    for sql_name in sql_names:
-        fn_sql = fn_dir / sql_name
-        fn_csv = fn_dir_csv / (sql_name[:-3] + "csv")
-        if fn_csv.exists():
-            print(f"{fn_csv} exists, skipping")
-            continue
-        df, data_bad = sql_to_df(fn_sql)
-        df.to_csv(fn_csv, index=False)
-
-    fn_tar = fn_dir / "csv.tar.gz"
-    if fn_tar.exists():
-        print(f"{fn_tar} exists, skipping")
-    else:
-        with tarfile.open(fn_tar, "w:gz") as tar:
-            tar.add(fn_dir / "csv", arcname='csv')
+    download_pipeline(fn_url, DATA_DIR / fn_tar, sql_names=sql_names, overwrite=False, cleanup=False)
+    convert_pipeline(fn_dir, fn_dir_csv, sql_names=sql_names)
--- src/download.py
+++ src/download.py
@@ -1,10 +1,7 @@
 import logging
-import tarfile
 from pathlib import Path
-from typing import List
 
 import wget
-from tqdm import tqdm
 
 
 def download_file(url: str, fn: Path, overwrite=False) -> bool:
@@ -19,29 +16,3 @@ def download_file(url: str, fn: Path, overwrite=False) -> bool:
         return False
     wget.download(url, fn.as_posix())
     return True
-
-
-def unzip_tar_bz2(fn: Path, sql_names: List[str]):
-    """ Unzips the file"""
-    logging.info(f"Unzipping File {fn}")
-    with tarfile.open(fn.as_posix(), "r:bz2") as tar:
-        for sql_name in tqdm(sql_names, desc='Extracting Files'):
-            tar.extract(sql_name, fn.parent.as_posix())
-
-
-def download_pipeline(url: str, fn: Path, sql_names: List[str], overwrite=False, cleanup=False):
-    """ Downloads the database files
-
-    Args:
-        url: https://data.ppy.sh... url
-        fn: File Name to download to
-        sql_names: SQL files to extract
-        overwrite: Whether to overwrite the tar gz2
-        cleanup: Deletes the tar gz2
-
-    """
-    if download_file(url, fn, overwrite):
-        unzip_tar_bz2(fn, sql_names)
-        if cleanup:
-            logging.info(f"Cleaning up file {fn}")
-            fn.unlink()
