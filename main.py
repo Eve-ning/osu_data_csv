a85e396142e22309e259086347cd20f8a5a26cd2
Evening <johnchangqi@hotmail.com>
2022-12-12 10:47:21 +0800

    Create additional function excluding click wrappers

--- main.py
+++ main.py
@@ -22,20 +22,6 @@ default_sql_names = [
 ]
 
 
-@click.command()
-@click.option('--year_month', '-y', default=datetime.now().strftime("%Y_%m"),
-              prompt=f"-y: Dataset Year & Month(YYYY_MM)")
-@click.option('--mode', '-d', default="mania", prompt=f"-d: Dataset Mode")
-@click.option('--set', '-s', default="1000", prompt=f"-s: Dataset Top ____ (1000 or 10000)")
-@click.option('--dl_dir', '-l', default="data/", prompt=f"-l: Folder to download files to")
-@click.option('--sql_names', '-n', default=",".join(default_sql_names[:4]),
-              prompt=f"-n: SQL Files to convert, separated by commas. <MODE> is substituted for --mode. "
-                     f"Convert all files if 'ALL'")
-@click.option('--cleanup', '-c', default="N",
-              prompt=f"-c: Whether to cleans up downloaded tar.bz2 and sql files after execution (Y/N)")
-@click.option('--zip_csv_files', '-z', default="N",
-              prompt=f"-c: Whether to zip the csv files in a tar.bz2 after conversion (Y/N)")
-@click.option('--bypass_confirm', '-q', default="N")
 def get_dataset(year_month: str, mode: str, set: str, dl_dir: str,
                 bypass_confirm: str, sql_names: str, cleanup: str, zip_csv_files: str):
     fn = f"{year_month}_01_performance_{mode}_top_{set}"
@@ -74,6 +60,25 @@ def get_dataset(year_month: str, mode: str, set: str, dl_dir: str,
     pipeline(fn, dl_dir, sql_names,
              cleanup=cleanup == 'Y',
              zip_csv_files=zip_csv_files == 'Y')
+
+
+@click.command()
+@click.option('--year_month', '-y', default=datetime.now().strftime("%Y_%m"),
+              prompt=f"-y: Dataset Year & Month(YYYY_MM)")
+@click.option('--mode', '-d', default="mania", prompt=f"-d: Dataset Mode")
+@click.option('--set', '-s', default="1000", prompt=f"-s: Dataset Top ____ (1000 or 10000)")
+@click.option('--dl_dir', '-l', default="data/", prompt=f"-l: Folder to download files to")
+@click.option('--sql_names', '-n', default=",".join(default_sql_names[:4]),
+              prompt=f"-n: SQL Files to convert, separated by commas. <MODE> is substituted for --mode. "
+                     f"Convert all files if 'ALL'")
+@click.option('--cleanup', '-c', default="N",
+              prompt=f"-c: Whether to cleans up downloaded tar.bz2 and sql files after execution (Y/N)")
+@click.option('--zip_csv_files', '-z', default="N",
+              prompt=f"-c: Whether to zip the csv files in a tar.bz2 after conversion (Y/N)")
+@click.option('--bypass_confirm', '-q', default="N")
+def get_dataset_click(year_month: str, mode: str, set: str, dl_dir: str,
+                      bypass_confirm: str, sql_names: str, cleanup: str, zip_csv_files: str):
+    get_dataset(year_month, mode, set, dl_dir, bypass_confirm, sql_names, cleanup, zip_csv_files)
 
 
 if __name__ == '__main__':
