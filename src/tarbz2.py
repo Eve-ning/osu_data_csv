369393394ac49910ccb47ccfdf8038dc80bed51e
Eve-ning <dev_evening@hotmail.com>
2023-02-12 19:14:40 +0800

    Selectively untar

--- src/tarbz2.py
+++ src/tarbz2.py
@@ -2,19 +2,14 @@ import logging
 import tarfile
 from pathlib import Path
 
+from src.conf import FILE_CONFIGS
+
 
 def unzip_tar_bz2(tar_file: Path):
-    """ Unzips the file"""
+    """ Unzips the file """
     logging.info(f"Unzipping File {tar_file}")
     with tarfile.open(tar_file.as_posix(), "r:bz2") as tar:
-        tar.extractall(tar_file.parent)
-
-
-def zip_tar_bz2(tar_file: Path, tar_target: Path, archive_name: str = 'csv'):
-    """ Zips the directory """
-    if tar_file.exists():
-        logging.info(f"{tar_file} exists, skipping")
-    else:
-        logging.info(f"Zipping to {tar_file}")
-        with tarfile.open(tar_file, "w:gz") as tar:
-            tar.add(tar_target, arcname=archive_name)
+        for member in tar.getmembers():
+            if any([k in member.name for k in FILE_CONFIGS.keys()]):
+                print(f"Extracting: {member.name}")
+                tar.extract(member)
