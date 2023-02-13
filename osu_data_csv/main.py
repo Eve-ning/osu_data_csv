c3661926e5739dce3f53a41d6c8979083722d325
Eve-ning <dev_evening@hotmail.com>
2023-02-13 22:34:05 +0800

    Add draft for sql filter

--- osu_data_csv/main.py
+++ osu_data_csv/main.py
@@ -3,12 +3,13 @@ from datetime import datetime
 from pathlib import Path
 
 import click
+import yaml
 
 from .pipelines import pipeline
 
 
 def get_dataset(year_month: str, mode: str, set: str, dl_dir: str,
-                bypass_confirm: str, cleanup: str):
+                bypass_confirm: str, cleanup: str, sql_filter: str):
     fn = f"{year_month}_01_performance_{mode}_top_{set}"
     dl_dir_abs = Path(dl_dir).absolute().as_posix()
 
@@ -25,21 +26,24 @@ def get_dataset(year_month: str, mode: str, set: str, dl_dir: str,
         print("Aborting.")
         return
 
+
     print("Proceeding to Download ...")
-    pipeline(fn, dl_dir, cleanup=cleanup == 'Y', mode=mode)
+    pipeline(fn, dl_dir, cleanup=cleanup == 'Y', mode=mode)#, sql_filter=sql_filter)
 
 
 @click.command()
 @click.option('--year_month', '-y', default=datetime.now().strftime("%Y_%m"),
               prompt=f"-y: Dataset Year & Month(YYYY_MM)")
-@click.option('--mode', '-d', default="mania", prompt=f"-d: Dataset Mode (osu/taiko/catch/mania)")
+@click.option('--mode', '-d', default="catch", prompt=f"-d: Dataset Mode (osu/taiko/catch/mania)")
 @click.option('--set', '-s', default="1000", prompt=f"-s: Dataset Top ____ (1000 or 10000)")
 @click.option('--dl_dir', '-l', default="data/", prompt=f"-l: Folder to download files to")
 @click.option('--cleanup', '-c', default="N",
               prompt=f"-c: Whether to cleans up downloaded tar.bz2 and sql files after execution (Y/N)")
 @click.option('--bypass_confirm', '-q', default="N")
-def get_dataset_click(year_month: str, mode: str, set: str, dl_dir: str, bypass_confirm: str, cleanup: str):
-    get_dataset(year_month, mode, set, dl_dir, bypass_confirm, cleanup)
+@click.option('--sql_filter', '-f', default="")
+def get_dataset_click(year_month: str, mode: str, set: str, dl_dir: str,
+                      bypass_confirm: str, cleanup: str, sql_filter: str):
+    get_dataset(year_month, mode, set, dl_dir, bypass_confirm, cleanup, sql_filter)
 
 
 def main():
