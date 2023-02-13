998f720f71cb54340cbe3bfbe23df4e331415282
Eve-ning <dev_evening@hotmail.com>
2023-02-13 22:33:57 +0800

    Restructure how mapping is done

--- osu_data_csv/conf.py
+++ osu_data_csv/conf.py
@@ -1,5 +1,11 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
 from datetime import datetime
 from pathlib import Path
+from typing import Callable
+
+import yaml
 
 DATA_DIR = Path(__file__).parents[1] / "data"
 
@@ -10,106 +16,45 @@ mode_mapping = {
     'mania': '_mania',
 }
 
+type_mapping = {
+    'str': str,
+    'int': int,
+    'float': float,
+    'datetime': datetime.fromisoformat
+}
 
-def get_file_configs(mode: str):
-    """ Retrieves the File Configuration for a particular mode
 
-    Notes:
-        This can be easily outdated due to the ever-changing database schema
-    """
+@dataclass
+class Column:
+    name: str
+    dtype: Callable
+    include: bool = True
+
+
+THIS_DIR = Path(__file__).parent
+
+
+def get_mapping(mode: str, mapping: dict[str, list[list]] = "", ignore: dict[str, list[str]] = "") -> dict[
+    str, list[Column]]:
+    """ Retrieves the File Configuration for a particular mode """
+    with open(THIS_DIR / 'default_mapping.yaml', 'r') as f:
+        mapping: dict[str, list[list]] = yaml.safe_load(f)
+
+    with open(THIS_DIR / 'ignore_mapping.yaml', 'r') as f:
+        ignore: dict[str, list[str]] = yaml.safe_load(f)
+
+    for _, sql_map in mapping.items():
+        for e, (column, dtype, include) in enumerate(sql_map):
+            sql_map[e] = Column(column, type_mapping[dtype], include)
+
+    for ignore_sql, ignore_columns in ignore.items():
+        for ignore_column in ignore_columns:
+            next(filter(lambda x: x.name == ignore_column,
+                        mapping[ignore_sql])).include = False
+
     mode = mode_mapping[mode]
-    return {
-        "osu_beatmap_difficulty.sql":
-            [
-                ('beatmap_id', int),
-                ('mode', int),
-                ('mods', int),
-                ('diff_unified', float),
-                ('last_update', datetime.fromisoformat)
-            ],
-        "osu_beatmaps.sql":
-            [
-                ('beatmap_id', int),
-                ('beatmapset_id', int),
-                ('user_id', int),
-                ('filename', str),
-                ('checksum', str),
-                ('version', str),
-                ('total_length', float),
-                ('hit_length', int),
-                ('countTotal', int),
-                ('countNormal', int),
-                ('countSlider', int),
-                ('countSpinner', int),
-                ('diff_drain', float),
-                ('diff_size', float),
-                ('diff_overall', float),
-                ('diff_approach', float),
-                ('playmode', int),
-                ('approved', int),
-                ('last_update', datetime.fromisoformat),
-                ('difficultyrating', float),
-                ('playcount', int),
-                ('passcount', int),
-                ('youtube_preview', str),
-                ('score_version', int),
-                ('deleted_at', datetime.fromisoformat),
-                ('bpm', float),
-            ],
-        f"osu_scores{mode}_high.sql":
-            [
-                ('score_id', int),
-                ('beatmap_id', int),
-                ('user_id', int),
-                ('score', int),
-                ('maxcombo', int),
-                ('rank', str),
-                ('count50', int),
-                ('count100', int),
-                ('count300', int),
-                ('countmiss', int),
-                ('countgeki', int),
-                ('countkatu', int),
-                ('perfect', int),
-                ('enabled_mods', int),
-                ('date', datetime.fromisoformat),
-                ('pp', float),
-                ('replay', int),
-                ('hidden', int),
-                ('country_acronym', str),
-            ],
-        f"osu_user_stats{mode}.sql":
-            [
-                ('user_id', int),
-                ('count300', int),
-                ('count100', int),
-                ('count50', int),
-                ('countMiss', int),
-                ('accuracy_total', int),
-                ('accuracy_count', int),
-                ('accuracy', float),
-                ('playcount', int),
-                ('ranked_score', int),
-                ('total_score', int),
-                ('x_rank_count', int),
-                ('xh_rank_count', int),
-                ('s_rank_count', int),
-                ('sh_rank_count', int),
-                ('a_rank_count', int),
-                ('rank', int),
-                ('level', float),
-                ('replay_popularity', int),
-                ('fail_count', int),
-                ('exit_count', int),
-                ('max_combo', int),
-                ('country_acronym', str),
-                ('rank_score', float),
-                ('rank_score_index', int),
-                ('rank_score_exp', float),
-                ('rank_score_index_exp', int),
-                ('accuracy_new', float),
-                ('last_update', datetime.fromisoformat),
-                ('last_played', datetime.fromisoformat),
-                ('total_seconds_played', int),
-            ]
-    }
+
+    for k, v in list(mapping.items()):
+        mapping[k.format(mode=mode)] = mapping.pop(k)
+
+    return mapping
