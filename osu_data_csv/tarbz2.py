dcd325c17a51a297d92e984a6ba0e34aa148b64d
Eve-ning <dev_evening@hotmail.com>
2023-02-14 10:14:14 +0800

    Make unzip more generic

--- osu_data_csv/tarbz2.py
+++ osu_data_csv/tarbz2.py
@@ -2,14 +2,12 @@ import logging
 import tarfile
 from pathlib import Path
 
-from .conf import get_mapping
 
-
-def unzip_tar_bz2(tar_file: Path, mode: str):
+def unzip_tar_bz2(tar_file: Path, files_to_extract: list[str]):
     """ Unzips the file """
     logging.info(f"Unzipping File {tar_file}")
     with tarfile.open(tar_file.as_posix(), "r:bz2") as tar:
         for member in tar.getmembers():
-            if any([k in member.name for k in get_mapping(mode).keys()]):
+            if any([k in member.name for k in files_to_extract]):
                 print(f"Extracting: {member.name}")
                 tar.extract(member, tar_file.parent.as_posix())
