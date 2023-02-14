83d3a8b75bf9f21e478349fb4d2c86ea03ca77bb
Eve-ning <dev_evening@hotmail.com>
2023-02-14 10:51:35 +0800

    Improve documentation of terminal prompt and add ignore_path

--- osu_data_csv/main.py
+++ osu_data_csv/main.py
@@ -9,7 +9,7 @@ from .pipelines import pipeline
 
 
 def get_dataset(year_month: str, mode: str, set: str, dl_dir: str,
-                bypass_confirm: str, cleanup: str, sql_filter: str):
+                bypass_confirm: str, cleanup: str, ignore_path: str):
     fn = f"{year_month}_01_performance_{mode}_top_{set}"
     dl_dir_abs = Path(dl_dir).absolute().as_posix()
 
@@ -26,24 +26,49 @@ def get_dataset(year_month: str, mode: str, set: str, dl_dir: str,
         print("Aborting.")
         return
 
-
     print("Proceeding to Download ...")
-    pipeline(fn, dl_dir, cleanup=cleanup == 'Y', mode=mode)#, sql_filter=sql_filter)
+    pipeline(fn, dl_dir, cleanup=cleanup == 'Y', mode=mode, ignore_path=ignore_path)
 
 
 @click.command()
-@click.option('--year_month', '-y', default=datetime.now().strftime("%Y_%m"),
-              prompt=f"-y: Dataset Year & Month(YYYY_MM)")
-@click.option('--mode', '-d', default="catch", prompt=f"-d: Dataset Mode (osu/taiko/catch/mania)")
-@click.option('--set', '-s', default="1000", prompt=f"-s: Dataset Top ____ (1000 or 10000)")
-@click.option('--dl_dir', '-l', default="data/", prompt=f"-l: Folder to download files to")
-@click.option('--cleanup', '-c', default="N",
-              prompt=f"-c: Whether to cleans up downloaded tar.bz2 and sql files after execution (Y/N)")
-@click.option('--bypass_confirm', '-q', default="N")
-@click.option('--sql_filter', '-f', default="")
+@click.option('--year_month', '-y',
+              default=datetime.now().strftime("%Y_%m"),
+              prompt=f"-y: Dataset Year & Month(YYYY_MM)".upper(),
+              help="Year Month in the YYYY_MM format")
+@click.option('--mode', '-d',
+              default="catch",
+              prompt=f"-d: Dataset Mode (osu/taiko/catch/mania)",
+              help="Game Mode. Must be either osu/taiko/catch/mania")
+@click.option('--set', '-s',
+              default="1000",
+              prompt=f"-s: Dataset Top ____ (1000 or 10000)",
+              help="Top-N player scores. Must be either 1000 or 10000")
+@click.option('--dl_dir', '-l',
+              default="data/",
+              prompt=f"-l: Folder to download files to",
+              help="The folder to download the zip, extract, and convert to")
+@click.option('--cleanup', '-c',
+              default="N",
+              prompt=f"-c: Whether to cleans up downloaded tar.bz2 and sql files after execution (Y/N)",
+              help="Whether to clean up the downloads after the script is complete")
+@click.option('--bypass_confirm', '-q',
+              default="N",
+              help="Whether to bypass the confirmation. Use this to skip the final prompt")
+@click.option('--ignore_path', '-i',
+              default="",
+              help="Path to the Ignore YAML. The format must be exactly as shown in the README.md. Optional ")
 def get_dataset_click(year_month: str, mode: str, set: str, dl_dir: str,
-                      bypass_confirm: str, cleanup: str, sql_filter: str):
-    get_dataset(year_month, mode, set, dl_dir, bypass_confirm, cleanup, sql_filter)
+                      bypass_confirm: str, cleanup: str, ignore_path: str):
+    """ Downloads, Extracts and converts the .sql dataset into .csv
+
+    Notes:
+        --ignore_path is optional.
+        You can use this if you want to speed up the conversion.
+        Please refer to the example in the README.md or in
+        https://github.com/Eve-ning/osu_data_csv/blob/master/README.md
+
+    """
+    get_dataset(year_month, mode, set, dl_dir, bypass_confirm, cleanup, ignore_path)
 
 
 def main():
