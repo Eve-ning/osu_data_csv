9435e53a7e5536eb53ae8886d4f57740b0707a6c
Eve-ning <dev_evening@hotmail.com>
2023-02-14 10:36:36 +0800

    Pull yaml paths to argument level

--- osu_data_csv/conf.py
+++ osu_data_csv/conf.py
@@ -7,8 +7,6 @@ from typing import Callable
 
 import yaml
 
-DATA_DIR = Path(__file__).parents[1] / "data"
-
 mode_mapping = {
     'osu': '',
     'taiko': '_taiko',
@@ -31,30 +29,54 @@ class Column:
     include: bool = True
 
 
-THIS_DIR = Path(__file__).parent
+def get_mapping(
+        mode: str, ignore_path: Path = None,
+        default_path: Path = Path(__file__).parent / 'default_mapping.yaml'
+) -> dict[str, list[Column]]:
+    """ Retrieves the File Configuration for a particular mode
 
+    Notes:
+        Usually, you don't need to change default_yaml
 
-def get_mapping(mode: str, mapping: dict[str, list[list]] = "", ignore: dict[str, list[str]] = "") -> dict[
-    str, list[Column]]:
-    """ Retrieves the File Configuration for a particular mode """
-    with open(THIS_DIR / 'default_mapping.yaml', 'r') as f:
-        mapping: dict[str, list[list]] = yaml.safe_load(f)
+    Args:
+        mode: Game Mode: (osu/taiko/catch/mania)
+        ignore_path: Path to ignore YAML
+        default_path: Path to default YAML
 
-    with open(THIS_DIR / 'ignore_mapping.yaml', 'r') as f:
-        ignore: dict[str, list[str]] = yaml.safe_load(f)
+    Returns:
+        The file mapping as a dict[str, list[Column]]
+        E.g. {'osu_beatmaps.sql': [Column(name='beatmap_id', dtype=<class int>, include=True), ... ], ...}
 
+    """
+    with open(default_path.as_posix(), 'r') as f:
+        mapping: dict[str, list[list[str | bool]]] = yaml.safe_load(f)
+
+    # Cast the lists within mapping to Column
     for _, sql_map in mapping.items():
         for e, (column, dtype, include) in enumerate(sql_map):
             sql_map[e] = Column(column, type_mapping[dtype], include)
 
-    for ignore_sql, ignore_columns in ignore.items():
-        for ignore_column in ignore_columns:
-            next(filter(lambda x: x.name == ignore_column,
-                        mapping[ignore_sql])).include = False
+    # Explicitly specify the type change.
+    mapping: dict[str, list[Column]]
+
+    # Run only if we have items to ignore
+    if ignore_path is not None:
+        with open(ignore_path.as_posix(), 'r') as f:
+            ignore: dict[str, list[str]] = yaml.safe_load(f)
+
+        # We loop through ignore yaml and set .include to false
+        for ignore_sql, ignore_columns in ignore.items():
+            for ignore_column in ignore_columns:
+                # We loop through its columns and check if the .name is the ignore_column
+                # mapping[ignore_sql]: list[Column]
+                # x: Column
+                next(filter(lambda x: x.name == ignore_column,
+                            mapping[ignore_sql])).include = False
 
     mode = mode_mapping[mode]
 
     for k, v in list(mapping.items()):
+        # We reformat the {mode} tags to the actual mode
         mapping[k.format(mode=mode)] = mapping.pop(k)
 
     return mapping
