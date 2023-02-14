84beb520c430435dfc1c75ff0bba6ed82b24abb9
Eve-ning <dev_evening@hotmail.com>
2023-02-14 10:14:24 +0800

    Move get_mapping to a higher level

--- osu_data_csv/pipelines.py
+++ osu_data_csv/pipelines.py
@@ -3,32 +3,32 @@ from __future__ import annotations
 import logging
 from pathlib import Path
 
-from .conf import get_mapping
+from .conf import get_mapping, Column
 from .download import download_file
 from .parse_sql import parse_sql_file
 from .tarbz2 import unzip_tar_bz2
 
 
-def download_pipeline(url: str, fn_tar: Path, mode: str):
+def download_pipeline(url: str, fn_tar: Path, files_to_extract: list[str]):
     """ Downloads the database files
 
     Args:
         url: https://data.ppy.sh... url
         fn_tar: File Name to download to
-
+        files_to_extract: File names to extract from.
     """
     if download_file(url, fn_tar):
-        unzip_tar_bz2(fn_tar, mode)
+        unzip_tar_bz2(fn_tar, files_to_extract=files_to_extract)
 
 
-def convert_pipeline_csv(tar_dir: Path, csv_dir: Path, mode: str):
+def convert_pipeline_csv(tar_dir: Path, csv_dir: Path, mapping: dict[str, list[Column]]):
     """ Converts the sql files to csvs and
 
     Args:
         tar_dir: Directory of the SQLs to convert
         csv_dir: Directory of CSV output
+        mapping: Mapping information for all .sql files
     """
-    mapping = get_mapping(mode=mode)
     for sql_name, sql_mapping in mapping.items():
         # E.g. sql_name = "osu_beatmaps.sql"
         sql_file = tar_dir / sql_name
@@ -43,6 +43,17 @@ def convert_pipeline_csv(tar_dir: Path, csv_dir: Path, mode: str):
 
 
 def pipeline(fn: str, dl_dir: str, cleanup: bool, mode: str):
+    """ Runs the download, extract and convert pipeline
+
+    Args:
+        fn: File name of the .tar.bz2 to be downloaded from the data warehouse (excluding .tar.bz2)
+        dl_dir: Download directory
+        cleanup: Whether to clean up the .sql and .tar.bz2 upon completion
+        mode: Game Mode
+
+    Returns:
+
+    """
     dl_dir = Path(dl_dir)
     tar_name = fn + ".tar.bz2"
     tar_dir = dl_dir / fn
@@ -51,8 +62,9 @@ def pipeline(fn: str, dl_dir: str, cleanup: bool, mode: str):
     csv_dir.mkdir(parents=True, exist_ok=True)
     tar_url = fr"https://data.ppy.sh/{tar_name}"
 
-    download_pipeline(tar_url, tar_file, mode=mode)
-    convert_pipeline_csv(tar_dir, csv_dir=csv_dir, mode=mode)
+    mapping = get_mapping(mode=mode)
+    download_pipeline(tar_url, tar_file, files_to_extract=list(mapping.keys()))
+    convert_pipeline_csv(tar_dir, csv_dir=csv_dir, mapping=mapping)
 
     if cleanup:
         logging.info("Cleaning Up & Removing Files...")
